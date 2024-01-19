c3b614edf92009b8a8297d5dc5a7fe38c519a50f
Gianfranco De Vettori <Ako@Akos-Mac-2.local>
2024-01-18 18:44:06 -0700

    starting shapes.js code

--- index.js
+++ index.js
@@ -30,3 +30,42 @@ function promptUser() {
     },
   ]);
 }
+
+function generateLogo() {
+    promptUser().then(answers => {
+      const { text, textColor, shape, shapeColor } = answers;
+      let logo;
+  
+      switch (shape) {
+        case 'Circle':
+          logo = new Circle();
+          break;
+        case 'Triangle':
+          logo = new Triangle();
+          break;
+        case 'Square':
+          logo = new Square();
+          break;
+        default:
+          console.log('Invalid shape selection.');
+          return;
+      }
+
+      logo.setColor(shapeColor);
+      logo.setTextColor(textColor);
+      logo.setText(text);
+
+      const svg = logo.render();
+      const logoPath = "./examples/logo.svg";
+
+      fs.writeFile(logoPath, svg, err => {
+        if (err) {
+          console.error('An error occurred while generating the logo:', err);
+        } else {
+          console.log('Generated logo.svg');
+        }
+      });
+    });
+  }
+  
+  generateLogo();
